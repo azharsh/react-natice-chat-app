f0c7847783f23a02899917481d57a884a930403e
azhar <acagntng@gmail.com>
2019-06-15 22:36:10 +0700

    first commit

---
+++ app/configs/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFg0vtCJIy7Vf2GMXM4xC-coq4t09W6Fo",
+    authDomain: "rnchat-9c590.firebaseapp.com",
+    databaseURL: "https://rnchat-9c590.firebaseio.com",
+    projectId: "rnchat-9c590",
+    storageBucket: "rnchat-9c590.appspot.com",
+    messagingSenderId: "189496164973",
+    appId: "1:189496164973:web:cc1363e6b3f59e39"
+};
